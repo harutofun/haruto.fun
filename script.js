5af6963f5ed318e50c2c80f6651633a30fd1201d
haruto <harutobessa@gmail.com>
2025-02-24 19:21:32 -0300

    added buttons and finished bluesky

--- script.js
+++ script.js
@@ -15,9 +15,9 @@ setInterval(function() {
     status_gif_.src = status_gifs[random];
 }, 6000);
 
-// tstaus progress bar
-const totalIssues = 6;
-const completedIssues = 1;
+// status progress bar
+const totalIssues = 20;
+const completedIssues = 2;
 const percentage = totalIssues > 0 ? Math.round((completedIssues / totalIssues) * 100) : 0;
 document.getElementById('progress-label').textContent = `${completedIssues}/${totalIssues}`;
 document.getElementById('progress-percentage').textContent = `${percentage}%`;
@@ -29,6 +29,49 @@ for (let i = 0; i < totalIssues; i++) {
   if (i < completedIssues) { segment.classList.add('completed');}
   progressBar.appendChild(segment);
 }
+
+// status container parallax movement
+const status_div = document.querySelector('.status_div');
+document.addEventListener('mousemove', (event) => {
+const x = (event.clientX / window.innerWidth) * 2;
+const y = (event.clientY / window.innerHeight) * 2;
+status_div.style.transform = `translate(${x - 1}%, ${y - 1}%)`;
+});
+
+
+
+// bluesky container parallax movement
+const bluesky_embed = document.querySelector('.bluesky_embed');
+document.addEventListener('mousemove', (event) => {
+const x = (event.clientX / window.innerWidth) * 2;
+const y = (event.clientY / window.innerHeight) * 2;
+bluesky_embed.style.transform = `translate(${x - 1}%, ${y - 1}%)`;
+});
+const bluesky_tab = document.querySelector('.bluesky_tab');
+document.addEventListener('mousemove', (event) => {
+const x = (event.clientX / window.innerWidth) * 2;
+const y = (event.clientY / window.innerHeight) * 2;
+bluesky_tab.style.transform = `translate(${x - 1}%, ${y - 1}%)`;
+});
+const bluesky_tab_bg = document.querySelector('.bluesky_tab_bg');
+document.addEventListener('mousemove', (event) => {
+const x = (event.clientX / window.innerWidth) * 4;
+const y = (event.clientY / window.innerHeight) * 4;
+bluesky_tab_bg.style.transform = `translate(${x - 2}%, ${y - 2}%)`;
+});
+const steam_button = document.querySelector('.steam_button');
+document.addEventListener('mousemove', (event) => {
+const x = (event.clientX / window.innerWidth) * 4;
+const y = (event.clientY / window.innerHeight) * 4;
+steam_button.style.transform = `translate(${x - 2}%, ${y - 2}%)`;
+});
+const itchio_button = document.querySelector('.itchio_button');
+document.addEventListener('mousemove', (event) => {
+const x = (event.clientX / window.innerWidth) * 5;
+const y = (event.clientY / window.innerHeight) * 5;
+itchio_button.style.transform = `translate(${x - 2.5}%, ${y - 2.5}%)`;
+});
+
 
 // hands parallax movement
 const hands_bottom = document.querySelector('.hands_bottom');
